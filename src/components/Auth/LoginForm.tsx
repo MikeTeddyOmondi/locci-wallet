8ea93ae87979aee7b3a08f4d7443d48628197af3
MikeTeddyOmondi <mike_omondi@outlook.com>
2024-12-16 15:15:49 +0300

    fix: updates - auth feat

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -49,7 +49,7 @@ export function LoginForm() {
 		} else {
 			notifications.show({
 				color: "red",
-				title: "Invalid Credentials",
+				title: "Login Error",
 				message: res.message,
 			});
 		}
